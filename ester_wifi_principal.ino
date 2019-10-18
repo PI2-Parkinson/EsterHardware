73774ebb01f358db039b120c1e59edb5a7cceb0a
anacamaro <31140359+anacamaro@users.noreply.github.com>
2019-10-17 21:57:19 -0300

    Add files via upload

---
+++ ester_wifi_principal.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+
+WiFiServer server(80);
+IPAddress IP(192,168,4,15);
+IPAddress mask = (255, 255, 255, 0);
+
+byte ledPin = 2;
+int test = 77;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("Wemos_AP", "Wemos_comm");
+  WiFi.softAPConfig(IP, IP, mask);
+  server.begin();
+  pinMode(ledPin, OUTPUT);
+  Serial.println();
+  Serial.println("AP_1.ino");
+  Serial.println("Server started.");
+  Serial.print("IP: ");     Serial.println(WiFi.softAPIP());
+  Serial.print("MAC:");     Serial.println(WiFi.softAPmacAddress());
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {return;}
+  digitalWrite(ledPin, LOW);
+  String request = client.readStringUntil('\r');
+  Serial.println("********************************");
+  Serial.println("From the station: " + request);
+  client.flush();
+  Serial.print("Byte sent to the station: ");
+  //Serial.println(client.println(request + "ca" + "\r"));
+  Serial.println(client.println("Hi, friend" + (String)test));
+  digitalWrite(ledPin, HIGH);
+}
---
+++ ester_wifi_strap.ino
@@ -0,0 +1,89 @@
+#define rezet D5  // D5 is more ok than D0
+#include <ESP8266WiFi.h>
+/*
+extern "C" {
+  #include "user_interface.h"
+}
+*/
+byte ledPin = 2;
+char ssid[] = "Wemos_AP";           // SSID of your AP
+char pass[] = "Wemos_comm";         // password of your AP
+
+IPAddress server(192,168,4,15);     // IP address of the AP
+WiFiClient client;
+
+int contor = 0;
+
+void setup() {
+  digitalWrite(rezet, HIGH);  // http://nicuflorica.blogspot.com/2015/11/termostat-controlat-de-pe-o-pagina-web-2.html
+  pinMode(rezet, OUTPUT);  // hard reset - https://github.com/esp8266/Arduino/issues/1622 
+  digitalWrite(rezet, HIGH);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW); 
+  delay(2000);
+  Serial.begin(9600);
+  delay(500);
+  WiFi.disconnect();
+  delay(500);
+  WiFi.mode(WIFI_STA);
+   delay(500);
+  WiFi.begin(ssid, pass);           // connects to the WiFi AP
+  Serial.println();
+  Serial.println("Connection to the AP");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println("Connected");
+  Serial.println("ST_1.ino");
+  Serial.print("LocalIP:"); Serial.println(WiFi.localIP());
+  Serial.println("MAC:" + WiFi.macAddress());
+  Serial.print("Gateway:"); Serial.println(WiFi.gatewayIP());
+  Serial.print("AP MAC:"); Serial.println(WiFi.BSSIDstr());
+
+}
+
+void loop() {
+  client.connect(server, 80);
+  digitalWrite(ledPin, LOW);
+  Serial.println("********************************");
+  //Serial.print("Byte sent to the AP: ");
+  Serial.println(client.print("Hi, boss!"));
+  String answer = client.readStringUntil('\r');
+  Serial.print("From the AP: " + answer);
+  int i, len;  // https://circuits4you.com/2018/03/09/how-to-convert-int-to-string-on-arduino/
+  int result=0; 
+  len = answer.length();
+  Serial.print("/");
+  Serial.println(len);
+
+  client.flush();
+  digitalWrite(ledPin, HIGH);
+  client.stop();
+  Serial.println();
+  Serial.print("Counter = ");
+  Serial.println(contor);
+  contor = contor +1;
+  delay(1000);
+ 
+if (WiFi.status() != WL_CONNECTED) 
+{
+  Serial.println("disconect from wifi - reset");
+  digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :)
+ }
+
+
+if (len < 1)
+  {                                   //   https://techtutorialsx.com/2017/12/29/esp8266-arduino-software-restart/
+   Serial.println("lost communications - reset");
+  digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :) 
+  } 
+ 
+ 
+if (contor > 200)
+  { 
+    Serial.println("obercount - reset");
+digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :)
+}
+}  // https://github.com/esp8266/Arduino/issues/1622
