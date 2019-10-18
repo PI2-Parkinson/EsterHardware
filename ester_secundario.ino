27ec4cff9bdba20cf26a350ee8f9003e71310849
anacamaro <31140359+anacamaro@users.noreply.github.com>
2019-10-18 13:43:06 -0300

    Update and rename ester_wifi_strap.ino to ester_secundario.ino

--- ester_secundario.ino
+++ ester_secundario.ino
@@ -12,16 +12,16 @@ char pass[] = "Wemos_comm";         // password of your AP
 IPAddress server(192,168,4,15);     // IP address of the AP
 WiFiClient client;
 
-int contor = 0;
+//int contor = 0;
 
 void setup() {
   digitalWrite(rezet, HIGH);  // http://nicuflorica.blogspot.com/2015/11/termostat-controlat-de-pe-o-pagina-web-2.html
   pinMode(rezet, OUTPUT);  // hard reset - https://github.com/esp8266/Arduino/issues/1622 
   digitalWrite(rezet, HIGH);
-  pinMode(ledPin, OUTPUT);
+ pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW); 
   delay(2000);
-  Serial.begin(9600);
+  Serial.begin(115200);//
   delay(500);
   WiFi.disconnect();
   delay(500);
@@ -45,45 +45,46 @@ void setup() {
 }
 
 void loop() {
-  client.connect(server, 80);
+  client.connect(server, 80);//conecta com o servidor
   digitalWrite(ledPin, LOW);
   Serial.println("********************************");
-  Serial.print("Byte sent to the AP: ");
+  Serial.print("Byte sent to the AP: \r");//
   Serial.println(client.print("Hi, boss!\r"));
   String answer = client.readStringUntil('\r');
-  Serial.print("From the AP: " + answer);
-  int i, len;  // https://circuits4you.com/2018/03/09/how-to-convert-int-to-string-on-arduino/
-  int result=0; 
-  len = answer.length();
-  Serial.print("/");
-  Serial.println(len);
+  Serial.print("Teste_secundario: " + answer);
+  //int i, len;  // https://circuits4you.com/2018/03/09/how-to-convert-int-to-string-on-arduino/
+  //int result=0; 
+ // len = answer.length();//
+  //Serial.print("/");//
+  //Serial.println(len);//
 
   client.flush();
   digitalWrite(ledPin, HIGH);
   client.stop();
   Serial.println();
-  Serial.print("Counter = ");
-  Serial.println(contor);
-  contor = contor +1;
+  //Serial.print("Counter = ");//
+  //Serial.println(contor);//
+  //contor = contor +1;//
   delay(1000);
  
 if (WiFi.status() != WL_CONNECTED) 
 {
   Serial.println("disconect from wifi - reset");
-  digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :)
+  digitalWrite(rezet, LOW); 
+  delay(100); //After than reset no need to delay but I put it for fun :)
  }
 
 
-if (len < 1)
-  {                                   //   https://techtutorialsx.com/2017/12/29/esp8266-arduino-software-restart/
-   Serial.println("lost communications - reset");
-  digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :) 
-  } 
+//if (len < 1)//
+  //{                                   //   https://techtutorialsx.com/2017/12/29/esp8266-arduino-software-restart/
+   //Serial.println("lost communications - reset");
+  //digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :) 
+  //} 
  
  
-if (contor > 200)
-  { 
-    Serial.println("obercount - reset");
-digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :)
-}
+//if (contor > 200)//
+  //{ 
+    //Serial.println("obercount - reset");
+//digitalWrite(rezet, LOW); delay(100); //After than reset no need to delay but I put it for fun :)
+//}
 }  // https://github.com/esp8266/Arduino/issues/1622
