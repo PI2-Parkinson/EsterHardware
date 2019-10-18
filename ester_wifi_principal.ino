d1bcfcc9af510fad51c8ec72069b8d183a8ca1d1
anacamaro <31140359+anacamaro@users.noreply.github.com>
2019-10-18 14:52:22 -0300

    Update ester_wifi_principal.ino

--- ester_wifi_principal.ino
+++ ester_wifi_principal.ino
@@ -1,21 +1,21 @@
 #include <ESP8266WiFi.h>
 
 WiFiServer server(80);
-IPAddress IP(192,168,4,15);
+IPAddress IP(192,168,4,10);
 IPAddress mask = (255, 255, 255, 0);
 
 byte ledPin = 2;
-int test = 77;
+String teste = "77";
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); 
   WiFi.mode(WIFI_AP);
   WiFi.softAP("Wemos_AP", "Wemos_comm");
   WiFi.softAPConfig(IP, IP, mask);
   server.begin();
   pinMode(ledPin, OUTPUT);
   Serial.println();
-  //Serial.println("AP_1.ino");
+  Serial.println("AP_1.ino");
   Serial.println("Server started.");
   Serial.print("IP: ");     Serial.println(WiFi.softAPIP());
   Serial.print("MAC:");     Serial.println(WiFi.softAPmacAddress());
@@ -25,12 +25,9 @@ void loop() {
   WiFiClient client = server.available();
   if (!client) {return;}
   digitalWrite(ledPin, LOW);
-  String request = client.readStringUntil('\r');//recebe mensagem do servidor
-  //Serial.println("********************************");
-  Serial.println("Teste_principal: " + request);
-  client.flush();
-  //Serial.print("Byte sent to the station: ");
-// Serial.println(client.println(request + "ca" + "\r"));
-  Serial.println(client.println("Hi, friend " + (String)test));//envia mensagem pro servidor
-  digitalWrite(ledPin, HIGH);
+  String request = client.readStringUntil('\r');
+  Serial.println("********************************");
+  Serial.println("Principal - Recebeu do secundário: " + request);
+  client.flush();+
+  client.print(teste + "\r");
 }
